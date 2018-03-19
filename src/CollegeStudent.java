7bed7ba4971b96b3928ad9fcd7ebd6cb1b44a123
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-18 22:04:02 -0500

    Update CollegeStudent.java

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,25 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Clayton Bruce
+  	Date: 3/18/2018
 */
 
 public class CollegeStudent {
 
 	public static void main(String[] args) {
-
+		
+		String studentYear = "Senior";
+		 
+		int numCourse =5;
+		
+		double gpa = 3.5;
+		
+	
+		System.out.println(gpa);
+		System.out.println(studentYear);
+		System.out.println(numCourse); 
+		
+		
 	}
 
 }
