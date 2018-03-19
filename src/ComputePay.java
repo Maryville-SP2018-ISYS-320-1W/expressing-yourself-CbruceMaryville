5f08f8808064386888c4605be830c3da2a613b25
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-18 22:15:46 -0500

    Update ComputePay.java

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -1,18 +1,23 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 3/18/2018
 */
 
 public class ComputePay {
 	public static void main(String[] args) {
+		
+		int hrsworked = 21;
+		double salary = 8.75;
+		double payday = (hrsworked*salary);
+		
 		System.out.println("My total hours worked:");
-		System.out.println(4 + 5 + 8 + 4);
+		System.out.println(hrsworked);
 		
-		System.out.println("My hourly salary:");
-		System.out.println("$8.75");
+		System.out.println("My hourly salary is:");
+		System.out.println(salary);
 		
-		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
+		System.out.println("My total pay in dollars is:");
+		System.out.println(payday);
 	}
 }
