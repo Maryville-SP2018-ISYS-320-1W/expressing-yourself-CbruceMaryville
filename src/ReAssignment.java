d9c7b93cda037d5e53ed99500bec0167abcb5643
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-18 22:08:58 -0500

    Update ReAssignment.java

--- src/ReAssignment.java
+++ src/ReAssignment.java
@@ -1,22 +1,33 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Clayton Bruce
+  	Date:  3/18/2018
 */
 
 /*
  	Your predicted values for variables a, b, and c
-	a => 
-	b =>
-	c => 
+	a => 6
+	b =>9
+	c => 16
  
  	After testing the statements in the main method, indicate above if your predictions were correct.
+ 	-Predictions were correct!!
 */
 
 public class ReAssignment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 5; 
+		int b = 10; 
+		int c = b;
+		a = a + 1; 
+		b = b - 1; 
+		c = c + a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 
 	}
 
