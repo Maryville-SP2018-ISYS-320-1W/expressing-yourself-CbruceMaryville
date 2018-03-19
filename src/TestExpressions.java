806073241f4a498bb93c4a69f38a1a6c1c211c8c
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-18 21:55:30 -0500

    Update TestExpressions.java

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -1,27 +1,35 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Clayton Bruce	
+  	Date: 3/18/2018
 */
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
+	1.8
+	2.11
+	3.12
+	4.4
+	5.2
+	6.11
+	7.4+7
+	8.7+4
  
  	After testing the expressions in the main method, indicate above if your predictions were correct.
+	-All of my predictions were correct but the #7,8
 */
 
 public class TestExpressions {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		System.out.println(2+3*4-6);
+		System.out.println(14/7*2+30/5+1);
+		System.out.println((12+6)/3*2);
+		System.out.println((238%10+3)%7);
+		System.out.println(6*7%4);
+		System.out.println(2+2+3+4);
+		System.out.println("2+2"+3+4);
+		System.out.println(3+4+"2+2");
 	}
 
 }
